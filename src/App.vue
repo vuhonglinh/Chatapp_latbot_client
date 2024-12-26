bf4b3ee84cb91722dd04887e66f2dbb1b7114fd8
Vu Hong Linh <vulinh18072k1@gmail.com>
2024-12-26 23:56:12 +0700

    update

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header></header>
+
+  <RouterView />
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Auth/Login.vue'
+import Register from '../views/Auth/Register.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+  ],
+})
+
+export default router
